6dbeaf625ad7541764a5afc96d4c09f0a02bada3
Peter Chenery <peter.chenery@anthonynolan.org>
2017-10-15 10:10:53 +0100

    Created function

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -10,71 +10,75 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            List<string> iList = new List<string>();
-            string output = string.Empty;
             Console.WriteLine("Please Enter the Maximum Number");
             int Max = int.Parse(Console.ReadLine());
 
-            for (int i = 1; i < Max+1; i++)
-            {
-                iList.Clear();
+            foreach (var result in Enumerable.Range(1, Max).Select(n => FizzBuzz(n)))
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
 
-                if (i % 11 == 0)
+        static private string FizzBuzz(int value)
+        {
+            List<string> iList = new List<string>();
+            string output = string.Empty;
+           
+            iList.Clear();
+
+            if (value % 11 == 0)
+            {
+                if (value % 13 == 0)
                 {
-                    if (i % 13 == 0)
-                    {
-                        iList.Add("Fezz");
-                    }
-                    iList.Add("Bong");
+                    iList.Add("Fezz");
                 }
-                else if (i % 3 == 0)
+                iList.Add("Bong");
+            }
+            else if (value % 3 == 0)
+            {
+                iList.Add("Fizz");
+                if (value % 13 == 0)
                 {
-                    iList.Add("Fizz");
-                    if (i % 13 == 0)
-                    {
-                        iList.Add("Fezz");
-                    }
-                    if (i % 5 == 0)
-                    {
-                        iList.Add("Buzz");
-                    }
-                    if ( i % 7 == 0)
-                    {
-                        iList.Add("Bang");
-                    }
-                 }
-                else if (i % 5 == 0)
+                    iList.Add("Fezz");
+                }
+                if (value % 5 == 0)
                 {
-                    if (i % 13 == 0)
-                    {
-                        iList.Add("Fezz");
-                    }
                     iList.Add("Buzz");
-                    if (i % 7 == 0)
-                    {
-                        iList.Add("Bang");
-                    }
                 }
-                else if (i % 7 == 0)
+                if ( value % 7 == 0)
                 {
-                    if (i % 13 == 0)
-                    {
-                        iList.Add("Fezz");
-                    }
                     iList.Add("Bang");
                 }
-                else
-                {
-                    iList.Add(i.ToString());
-                }
-                if (i % 17 == 0)
-                {
-                    iList.Reverse();
-                }
-                output = String.Join("", iList);
-                Console.WriteLine(output);
             }
-            Console.ReadLine();
+            else if (value % 5 == 0)
+            {
+                if (value % 13 == 0)
+                {
+                    iList.Add("Fezz");
+                }
+                iList.Add("Buzz");
+                if (value % 7 == 0)
+                {
+                    iList.Add("Bang");
+                }
+            }
+            else if (value % 7 == 0)
+            {
+                if (value % 13 == 0)
+                {
+                    iList.Add("Fezz");
+                }
+                iList.Add("Bang");
+            }
+            else
+            {
+                iList.Add(value.ToString());
+            }
+            if (value % 17 == 0)
+            {
+                iList.Reverse();
+            }
+            output = String.Join("", iList);
+            return output;
         }
     }
 }
